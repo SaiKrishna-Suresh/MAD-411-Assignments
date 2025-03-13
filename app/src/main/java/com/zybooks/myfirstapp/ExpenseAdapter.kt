f35fa3e17fb016dc25f55b1074ff161406301e5d
SaiKrishna Suresh <w0845777@myscc.ca>
2025-03-13 12:39:33 -0400

    Assinment 7

---
+++ app/src/main/java/com/zybooks/myfirstapp/ExpenseAdapter.kt
@@ -0,0 +1,50 @@
+package com.zybooks.myfirstapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ExpenseAdapter (
+    private val expenseList: MutableList<MainActivity.Expense>):
+        RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder>(){
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ExpenseAdapter.ExpenseViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.expense_item,parent,false)
+        return ExpenseViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ExpenseAdapter.ExpenseViewHolder,
+        position: Int,
+    ) {
+        val expense = expenseList[position]
+        holder.name.text = expense.name
+        holder.amount.text = "$${expense.amount}"
+        holder.date.text = expense.date
+
+        holder.deleteButton.setOnClickListener{
+            expenseList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position,expenseList.size)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return expenseList.size
+    }
+    class ExpenseViewHolder(view:View):RecyclerView.ViewHolder(view){
+        val name: TextView = view.findViewById(R.id.nameView)
+        val amount: TextView = view.findViewById(R.id.amountView)
+        val date: TextView = view.findViewById(R.id.dateView)
+        val deleteButton:Button = view.findViewById(R.id.deleteButton)
+    }
+
+}
--- app/src/main/java/com/zybooks/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/zybooks/myfirstapp/MainActivity.kt
@@ -3,29 +3,54 @@ package com.zybooks.myfirstapp
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editTextName: EditText
-    private lateinit var textResult: TextView
+    private lateinit var expenseName : EditText
+    private lateinit var expenseAmount: EditText
+    private lateinit var expenseDate: EditText
+    private lateinit var expenseButton: Button
+    private lateinit var recyclerView: RecyclerView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        editTextName = findViewById(R.id.editTextName)
-        textResult = findViewById(R.id.textResult)
+
+        expenseName = findViewById(R.id.expenseName)
+        expenseAmount = findViewById(R.id.expenseAmount)
+        expenseDate = findViewById(R.id.expenseDate)
+        expenseButton = findViewById(R.id.expenseButton)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val expenseList = mutableListOf<Expense>()
+        val adapter = ExpenseAdapter(expenseList)
+        recyclerView.adapter = adapter
+
+        expenseButton.setOnClickListener{
+            val name = expenseName.text.toString()
+            val amount = expenseAmount.text.toString()
+            val date = expenseDate.text.toString()
+
+            val expense = Expense(name,amount.toDouble(),date)
+            expenseList.add(expense)
+            adapter.notifyItemInserted(expenseList.size)
+
+            expenseName.text.clear()
+            expenseAmount.text.clear()
+            expenseDate.text.clear()
+
+        }
 
     }
+    class Expense (val name:String, val amount:Double,val date:String)
 
-    fun showName(view: View){
-        val name = editTextName.text.toString()
-        textResult.text = "Hello, $name!"
-
-    }
 }
